d6a8113b0e0734e2396e59eb8086b917c1100545
Gabriel Ferraz <sudoferraz@gmail.com>
2024-07-12 15:16:51 +0200

    Adds subscription to bonding curve

--- contracts/contracts/BondageCurve.sol
+++ contracts/contracts/BondageCurve.sol
@@ -10,7 +10,7 @@ import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ER
 contract BondageCurve is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 	uint256 public protocolTreasuryBalance;
 	uint256 public contentCreatorBalance;
-	uint256 public totalMintedTokens = 1;
+	uint256 public totalMintedTokens;
 	uint256 public constant reserveRatio = 1000000; // Reserve ratio in ppm (parts per million)
 	uint256 public constant supply = 1000000; // Total supply of tokens
 	uint256 public constant fixedSubscriptionPrice = 25; // in usdc
@@ -52,6 +52,8 @@ contract BondageCurve is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 
 	event SubscriptionPurchased(address indexed buyer);
 
+	event TokensBurned(uint256 amountBurned);
+
 	function pause() external onlyOwner {
 		_pause();
 	}
@@ -66,6 +68,7 @@ contract BondageCurve is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 			"Content Creator balance must be greater than 0 to withdraw"
 		);
 		usdc.transfer(msg.sender, contentCreatorBalance);
+		contentCreatorBalance = 0;
 	}
 
 	function protocolTreasuryBalanceWithdrawal() external onlyOwner {
@@ -74,5 +77,60 @@ contract BondageCurve is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 			"Protocol treasury balance must be greater than 0 to withdraw"
 		);
 		usdc.transfer(msg.sender, protocolTreasuryBalance);
+		protocolTreasuryBalance = 0;
+	}
+
+	function purchaseTokens(uint256 usdcAmount) public {
+		require(
+			usdc.balanceOf(msg.sender) > 0,
+			"Must have USDc to purchase tokens"
+		);
+
+		require(
+			usdc.allowance(msg.sender, address(this)) > usdcAmount,
+			"Not enough allowance to purchase tokens"
+		);
+
+		usdc.transferFrom(msg.sender, address(this), usdcAmount);
+
+		uint256 tokensToMint = calculatePurchaseReturnPrice(usdcAmount);
+		protocolTreasuryBalance += (usdcAmount * 20) / 100;
+		contentCreatorBalance += (usdcAmount * 80) / 100;
+		totalMintedTokens += tokensToMint;
+		_mint(msg.sender, tokensToMint);
+
+		emit TokensPurchased(msg.sender, usdcAmount, tokensToMint);
+	}
+
+	function calculatePurchaseReturnPrice(
+		uint256 usdcAmount
+	) public view returns (uint256) {
+		return (usdcAmount * reserveRatio) / (totalMintedTokens + supply);
+	}
+
+	function purchaseSubscription() public {
+		uint256 subscriptionPriceInTokens = calculateSubscriptionPrice();
+
+		require(
+			this.balanceOf(msg.sender) >= subscriptionPriceInTokens,
+			"Not enough tokens to purchase subscription"
+		);
+		require(
+			this.allowance(msg.sender, address(this)) >=
+				subscriptionPriceInTokens,
+			"Not enough allowance to purchase subscription"
+		);
+
+		this.burnFrom(msg.sender, subscriptionPriceInTokens);
+
+		emit TokensBurned(subscriptionPriceInTokens);
+		emit SubscriptionPurchased(msg.sender);
+	}
+
+	function calculateSubscriptionPrice() public view returns (uint256) {
+		uint256 subscriptionPriceInTokens = calculatePurchaseReturnPrice(
+			fixedSubscriptionPrice
+		);
+		return subscriptionPriceInTokens;
 	}
 }
