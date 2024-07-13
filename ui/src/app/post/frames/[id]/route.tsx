59169939b2e63adea10a8b34686cae1be91c8da0
Gabriel Ferraz <sudoferraz@gmail.com>
2024-07-14 01:50:07 +0200

    Changes preview image

--- ui/src/app/post/frames/[id]/route.tsx
+++ ui/src/app/post/frames/[id]/route.tsx
@@ -9,102 +9,102 @@ import { Posts, Users } from "@prisma/client";
 import { fetchUserById } from "@/app/api/users/user.service";
 
 const handler = async (
-    req: NextRequest,
-    { params: { id } }: { params: { id: string } }
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
 ) => {
 
-    const post = await fetchPost(id);
-    if (post == null) {
-        return await create404Frame(req);
-    }
+  const post = await fetchPost(id);
+  if (post == null) {
+    return await create404Frame(req);
+  }
 
-    const user = await fetchUserById(post?.user_id);
-    if (user == null) {
-        return await create404Frame(req);
-    }
+  const user = await fetchUserById(post?.user_id);
+  if (user == null) {
+    return await create404Frame(req);
+  }
 
-    return createPreview(req, user, post)
+  return createPreview(req, user, post)
 };
 
 
 async function createPreview(
-    req: NextRequest,
-    user: Users,
-    posts: Posts
+  req: NextRequest,
+  user: Users,
+  posts: Posts
 ) {
-    const username: string = user.username ?? (user.display_name ? user.display_name : "");
-    const postId = posts.id;
+  const username: string = user.username ?? (user.display_name ? user.display_name : "");
+  const postId = posts.id;
 
-    const appUrl = process.env.APP_URL ? process.env.APP_URL : 'http://localhost:3000';
-    return await frames(async (ctx) => {
-        return {
-            image: (
-                <div tw="flex">
-                    <img
-                        width="100%"
-                        src={`${appUrl}/hidden-message.svg`}
-                        alt="Thumb"
-                    />
-                    <span
-                        style={{
-                            position: 'absolute',
-                            color: 'white',
-                            padding: '8px',
-                            textAlign: 'center',
-                            top: '80%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)',
-                        }}
-                    >
-                        {username}
-                    </span>
-                </div>
-            ),
-            imageOptions: {
-                aspectRatio: "1:1",
-            },
-            buttons: [
-                <Button action="post" target={`${postId}/postframe`}>
-                    👀 View Post
-                </Button>,
-                <Button
-                    action="link"
-                    target={
-                        `${appUrl}/sign-in`
-                    }
-                >
-                    Sign Up to OnlyFriends
-                </Button >,
-            ],
-            title: `${username} OnlyFriends Post`,
-        };
-    })(req);
+  const appUrl = process.env.APP_URL ? process.env.APP_URL : 'http://localhost:3000';
+  return await frames(async (ctx) => {
+    return {
+      image: (
+        <div tw="flex">
+          <img
+            width="100%"
+            src={`https://xkrrcuvbmddexwwj.public.blob.vercel-storage.com/hidden-message-E9mUdlnWTpB3w2wHiQCmmFvTebnALt.svg`}
+            alt="Thumb"
+          />
+          <span
+            style={{
+              position: 'absolute',
+              color: 'white',
+              padding: '8px',
+              textAlign: 'center',
+              top: '80%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          >
+            {username}
+          </span>
+        </div>
+      ),
+      imageOptions: {
+        aspectRatio: "1:1",
+      },
+      buttons: [
+        <Button action="post" target={`${postId}/postframe`}>
+          👀 View Post
+        </Button>,
+        <Button
+          action="link"
+          target={
+            `${appUrl}/sign-in`
+          }
+        >
+          Sign Up to OnlyFriends
+        </Button >,
+      ],
+      title: `${username} OnlyFriends Post`,
+    };
+  })(req);
 }
 
 async function create404Frame(req: NextRequest) {
-    return await frames(async (ctx) => {
-        return {
-            image: (
-                <div tw="flex">
-                    <span>Whoops Post not found</span>
-                </div>
-            ),
-            imageOptions: {
-                aspectRatio: "1:1",
-            },
-            buttons: [
-                <Button
-                    action="link"
-                    target={
-                        "https://github.com/builders-garden/open-frames-starter-framesjs"
-                    }
-                >
-                    Sign Up to OnlyFriends
-                </Button>,
-            ],
-            title: `OnlyFriends Frame`,
-        };
-    })(req);
+  return await frames(async (ctx) => {
+    return {
+      image: (
+        <div tw="flex">
+          <span>Whoops Post not found</span>
+        </div>
+      ),
+      imageOptions: {
+        aspectRatio: "1:1",
+      },
+      buttons: [
+        <Button
+          action="link"
+          target={
+            "https://github.com/builders-garden/open-frames-starter-framesjs"
+          }
+        >
+          Sign Up to OnlyFriends
+        </Button>,
+      ],
+      title: `OnlyFriends Frame`,
+    };
+  })(req);
 }
 
 
