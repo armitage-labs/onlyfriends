ec68cb3ab4bd245171d335c9595f28bb5126a980
Gabriel Ferraz <sudoferraz@gmail.com>
2024-07-14 01:33:02 +0200

    Adds styled header

--- ui/src/components/layout/header.tsx
+++ ui/src/components/layout/header.tsx
@@ -2,12 +2,24 @@
 
 import LogoutButton from "../authentication/logout-button";
 import WalletButton from "../authentication/wallet-button";
+import Image from "next/image";
 
 export default function Header() {
-    return (
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 justify-end">
-            <WalletButton></WalletButton>
-            <LogoutButton></LogoutButton>
-        </header>
-    )
+  return (
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 justify-between">
+
+      <div className="pl-16 items-center">
+        <Image
+          width={80}
+          height={80}
+          alt="OnlyFriends Logo"
+          src={"https://xkrrcuvbmddexwwj.public.blob.vercel-storage.com/onlyfiriends-logo-ySPTXfgTqsio390klIpl1w1y1XEW9r.svg"}
+        />
+      </div>
+      <div className="">
+        <WalletButton></WalletButton>
+        <LogoutButton></LogoutButton>
+      </div>
+    </header>
+  )
 }
--- ui/src/components/layout/sidebar.tsx
+++ ui/src/components/layout/sidebar.tsx
@@ -10,18 +10,15 @@ export default function Sidebar() {
       className={cn(`border-r lg:block w-72`)}
     >
       <div className="">
-        <div className="px-3 py-2">
-          <div className="space-y-1">
+        <div className="px-3 py-0">
+          <div className="">
             <div className="flex flex-col items-center">
               <Image
-                width={80}
-                height={80}
-                alt="OnlyFriends Logo"
-                src={"https://xkrrcuvbmddexwwj.public.blob.vercel-storage.com/onlyfiriends-logo-ySPTXfgTqsio390klIpl1w1y1XEW9r.svg"}
+                width={150}
+                height={150}
+                alt="OnlyFriends Text Logo"
+                src={"https://xkrrcuvbmddexwwj.public.blob.vercel-storage.com/text-logo-MumodmMCXKGGI3JpDeTv75ZtPpIBi8.svg"}
               />
-              <h2 className="mb-2 px-4 text-2xl font-semibold tracking-tight pb-4">
-                OnlyFriends
-              </h2>
             </div>
             <DashboardNav items={signedInNavItems} />
           </div>
